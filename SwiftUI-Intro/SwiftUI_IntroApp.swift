1068f06bf0f5428a16f849795fd31b7a5db85957
Yevhenii Pasko <v-ypasko@crestron.com>
2022-05-05 11:16:33 +0200

    Initial Commit

---
+++ SwiftUI-Intro/SwiftUI_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IntroApp.swift
+//  SwiftUI-Intro
+//
+//  Created by Ncube on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
